e3d397f70039029de1fed8e9dd61819c9ea3785c
amiralzd <123456amir78900@gmail.com>
2023-10-10 16:59:15 +0330

    responsive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-// Initialization for ES Users
-import {
-    Carousel,
-    initTE,
-  } from "tw-elements";
-  
-  initTE({ Carousel });
+function showburgerMenu(){
+  let x = document.getElementById("burger-Menu");
+  x.classList.add("burgerMenu-show");
+}
+
+function hideBurgerMenu(){
+  let x = document.getElementById("burger-Menu");
+  x.classList.remove("burgerMenu-show");
+}
+
+function MahsolatIcon(){
+  let d = document.getElementById("mahsolat-icon");
+  d.classList.toggle("mahsolat-show")
+}
