f4f8242fe89d67b6884cd906d9db38fc9daf60c1
amiralzd <123456amir78900@gmail.com>
2023-10-13 21:33:41 +0330

    slide andre-francois

--- js/script.js
+++ js/script.js
@@ -67,3 +67,48 @@ prevSlide.addEventListener("click", function () {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
+
+
+// for andre
+
+let currentSlide = 0;
+const slidess = document.querySelectorAll(".slides")
+const dots = document.querySelectorAll('.dot')
+
+const init = (n) => {
+  slidess.forEach((slide, index) => {
+    slide.style.display = "none"
+    dots.forEach((dot, index) => {
+      dot.classList.remove("active")
+    })
+  })
+  slidess[n].style.display = "block"
+  dots[n].classList.add("active")
+}
+document.addEventListener("DOMContentLoaded", init(currentSlide))
+const next = () => {
+  currentSlide >= slidess.length - 1 ? currentSlide = 0 : currentSlide++
+  init(currentSlide)
+}
+
+const prev = () => {
+  currentSlide <= 0 ? currentSlide = slidess.length - 1 : currentSlide--
+  init(currentSlide)
+}
+
+document.querySelector(".next").addEventListener('click', next)
+
+document.querySelector(".prev").addEventListener('click', prev)
+
+
+// setInterval(() => {
+//   next()
+// }, 5000);
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    console.log(currentSlide)
+    init(i)
+    currentSlide = i
+  })
+})
