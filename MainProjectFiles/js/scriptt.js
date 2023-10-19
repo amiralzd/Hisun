5c62911b2f655d89ee754e59411b5514f211d151
amiralzd <123456amir78900@gmail.com>
2023-10-19 20:58:02 +0330

    responsive | burger menu

--- MainProjectFiles/js/scriptt.js
+++ MainProjectFiles/js/scriptt.js
@@ -2,3 +2,13 @@ function showProducts(){
     let Products = document.getElementById("Products-show-hide");
     Products.classList.toggle("show-Products")
 }
+
+function burgermenu(){
+    let x = document.getElementById("burgermenu");
+    x.classList.add("burgerMenu-show");
+}
+
+function closeburgerMenu(){
+    let x = document.getElementById("burgermenu");
+    x.classList.remove("burgerMenu-show");
+}
